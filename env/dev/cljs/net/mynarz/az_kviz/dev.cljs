2f8327c0992b9d63f6f6393784791a57e6f72cb9
Jindrich Mynarz <mynarzjindrich@gmail.com>
2019-02-13 13:51:35 +0100

    Instrument cljs.spec during development

--- env/dev/cljs/net/mynarz/az_kviz/dev.cljs
+++ env/dev/cljs/net/mynarz/az_kviz/dev.cljs
@@ -1,11 +1,12 @@
 (ns ^:figwheel-no-load net.mynarz.az-kviz.dev
-  (:require
-    [net.mynarz.az-kviz.core :as core]
-    [devtools.core :as devtools]))
-
+  (:require [net.mynarz.az-kviz.core :as core]
+            [cljs.spec.test.alpha :as ts]
+            [devtools.core :as devtools]))
 
 (enable-console-print!)
 
 (devtools/install!)
 
 (core/init!)
+
+(ts/instrument)
